956abd186b5ff7e1f334b6425fe6e8814e6a5c54
Adrian <adrianc.crim@hotmail.com>
2024-05-10 21:21:55 +0200

    Commit de Adrian proyectoJavaActividadGit/src/adrianCarmonaCaballero

---
+++ proyectoJavaActividadGit/src/adrianCarmonaCaballero/Adrian.java
@@ -0,0 +1,14 @@
+package adrianCarmonaCaballero;
+
+public class Adrian {
+
+	public static void main(String[] args) {
+		saludo();
+	  }
+	    
+	  public static void saludo() {
+	        
+	      System.out.println("Hola, soy Adrián.");
+	 }
+
+	}
